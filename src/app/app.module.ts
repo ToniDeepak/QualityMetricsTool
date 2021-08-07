34a5a3bde8253fc913b9c018b7c43ae336e308aa
KarunaRohilla1 <m123kr@gmail.com>
2021-08-07 23:40:29 +0530

    UI Base Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QualityMetricsDetailsComponent } from './quality-metrics-details/quality-metrics-details.component';
 import { QualityMetricsDetailsFormComponent } from './quality-metrics-details/quality-metrics-details-form/quality-metrics-details-form.component';
-import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     QualityMetricsDetailsComponent,
-    QualityMetricsDetailsFormComponent
+    QualityMetricsDetailsFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BsDatepickerModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
